4f133cbe5dddb5a2837d40b6707512cefc00b302
FlorianBaumgartner <florian.b-h@bluewin.ch>
2022-04-13 22:48:21 +0200

    Added PyAudio Example

---
+++ RaspberryPi/PyAudioTest.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 22:32:19 2022
+
+@author: root
+"""
+
+"""
+PyAudio Example: Make a wire between input and output (i.e., record a
+few samples and play them back immediately).
+This is the callback (non-blocking) version.
+"""
+
+import pyaudio
+import time
+
+WIDTH = 2
+CHANNELS = 2
+RATE = 44100
+
+p = pyaudio.PyAudio()
+
+def callback(in_data, frame_count, time_info, status):
+    return (in_data, pyaudio.paContinue)
+
+for i in range(p.get_device_count()):
+    print(p.get_device_info_by_index(i))
+
+stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                output=True,
+                stream_callback=callback)
+
+stream.start_stream()
+
+while stream.is_active():
+    time.sleep(0.1)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
