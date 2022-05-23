d6d6f7cb17cc5b1a526666819e9ea99ebf62149a
Florian Baumgartner <florian.b-h@bluewin.ch>
2022-05-23 10:59:03 +0200

    GUI

--- AudioBeamformer/GUI/GUI.py
+++ AudioBeamformer/GUI/GUI.py
@@ -41,7 +41,7 @@ from PyQt5.QtQml  import QQmlApplicationEngine
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QUrl, pyqtProperty
 from pathlib import Path
 
-DEBUG = True
+DEBUG = False
 LINUX = (sys.platform == 'linux')
 sys.path.insert(0, os.path.dirname(__file__))
 sys.path.insert(0, os.path.dirname(__file__) + "/PyCVQML")
